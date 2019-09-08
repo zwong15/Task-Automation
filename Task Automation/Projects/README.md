8d47fb135b8d1e1b23521dcf5582bec1ca4a3ee7
Zach Wong <36214113+zwong15@users.noreply.github.com>
2019-09-07 23:18:56 -0500

    Add files via upload
    Python code for three mini automation projects

---
+++ Task Automation/Projects/README.md
@@ -0,0 +1,14 @@
+### Install: 
+```bash
+clone Github Repository
+cd ProjectInitializationAutomation
+pip install -r requirements.txt
+source ~/.my_commands.sh
+Then go to create.py and set the username and password to be your username and password.
+Also make sure to change all directories to your directories so it should be '/Users/<your username>/path/to/your/project'
+```
+
+### Usage:
+```bash
+To run the script type in 'create <name of your folder>'
+```
---
+++ Task Automation/Projects/create_project.py
@@ -0,0 +1,21 @@
+# Create Project Script
+
+import sys
+import os
+from github import Github
+
+path = "/Users/Z_Wong15/Documents/Projects/MyProjects/"
+
+username = "" #Insert your github username here
+password = "" #Insert your github password here
+
+def create():
+    folderName = str(sys.argv[1])
+    os.makedirs(path + str(folderName)
+    user = Github(username, password).get_user()
+    repo = user.create_repo(folderName)
+    print("Succesfully created repository {}".format(folderName))
+
+if __name__ == "__main__":
+    create()
+
